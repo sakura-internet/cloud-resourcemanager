2e016c4d643d1318785fbcf57bd8485fa739ed3f
Takuya Yoshimura <32727155+t-yoshimura25@users.noreply.github.com>
2019-07-31 10:28:51 +0900

    実行バージョン2に対応

--- tffile/server.tf
+++ tffile/server.tf
@@ -8,40 +8,42 @@
 # ----------------------------------------------------------
 # サーバーで利用するパブリックアーカイブ(CentOS7)
 # ----------------------------------------------------------
-data sakuracloud_archive "centos" {
-    os_type = "centos"          // "ubuntu" を指定するとUbuntuの最新安定版パブリックアーカイブ
+data "sakuracloud_archive" "centos" {
+  os_type = "centos" // "ubuntu" を指定するとUbuntuの最新安定版パブリックアーカイブ
 }
+
 # ----------------------------------------------------------
 # サーバー
 # ----------------------------------------------------------
 # ディスク作成
-resource "sakuracloud_disk" "disk01"{
-    name = "disk01"             // ディスク名の指定
-    plan      = "ssd"           // プランの指定
-    size      = 40              // 容量指定(GB)
-    source_archive_id = "${data.sakuracloud_archive.centos.id}" // アーカイブの設定
+resource "sakuracloud_disk" "disk01" {
+  name              = "disk01"                           // ディスク名の指定
+  plan              = "ssd"                              // プランの指定
+  size              = 40                                 // 容量指定(GB)
+  source_archive_id = data.sakuracloud_archive.centos.id // アーカイブの設定
 }
+
 # VM作成
 resource "sakuracloud_server" "server01" {
-    name = "server01"           // サーバ名の指定
-    disks = ["${sakuracloud_disk.disk01.id}"]
-    nic = "shared"              // 共有セグメントに接続
-    core = 2                    // CPUコアの指定
-    memory = 2                  // メモリ容量の指定
+  name   = "server01" // サーバ名の指定
+  disks  = [sakuracloud_disk.disk01.id]
+  nic    = "shared" // 共有セグメントに接続
+  core   = 2        // CPUコアの指定
+  memory = 2        // メモリ容量の指定
 
-    hostname = "your-host-name" // ホスト名の設定
-    password = "your-password"  // rootパスワードの設定
+  hostname = "your-host-name" // ホスト名の設定
+  password = "your-password"  // rootパスワードの設定
 }
 
 # ----------------------------------------------------------
 # シンプル監視
 # ----------------------------------------------------------
 # ping監視の例
-resource sakuracloud_simple_monitor "mymonitor" {
-  target = "${sakuracloud_server.server01.ipaddress}"
+resource "sakuracloud_simple_monitor" "mymonitor" {
+  target = sakuracloud_server.server01.ipaddress
 
-  health_check = {
-    protocol   = "ping"
+  health_check {
+    protocol = "ping"
   }
 
   notify_email_enabled = true
